0b72528d5ce01d8649e771d29804d0db4a6ebaa9
Peter Drake <drake@lclark.edu>
2016-06-09 10:11:41 -0700

    Changed Beetle tests to new style and updated POGIL.

--- Beetle/src/BeetleTest.java
+++ Beetle/src/BeetleTest.java
@@ -14,85 +14,166 @@ public class BeetleTest {
 	}
 
 	@Test
-	public void testAddBody() {
-		assertFalse(beetle.hasBody());
+	public void initiallyThereIsNoBody() {
+		assertFalse(beetle.hasBody());		
+	}
+	
+	@Test
+	public void canAddBodyIfThereIsNone() {
 		assertTrue(beetle.addBody());
-		assertTrue(beetle.hasBody());
-		// Trying to add a second body should fail
+		assertTrue(beetle.hasBody());		
+	}
+
+	@Test
+	public void cannotAddSecondBody() {
+		beetle.addBody();
 		assertFalse(beetle.addBody());
 	}
 
 	@Test
-	public void testAddHead() {
+	public void initiallyThereAreNoOtherParts() {
 		assertFalse(beetle.hasHead());
-		// Can't add a head without a body
+		assertFalse(beetle.hasTail());
+		assertEquals(0, beetle.getFeelers());
+		assertEquals(0, beetle.getEyes());
+		assertEquals(0, beetle.getLegs());
+	}
+
+	@Test
+	public void cannotAddHeadWithoutBody() {
 		assertFalse(beetle.addHead());
+	}
+
+	@Test
+	public void canAddHeadWithBody() {
 		beetle.addBody();
 		assertTrue(beetle.addHead());
 		assertTrue(beetle.hasHead());
-		// Trying to add a second head should fail
-		assertFalse(beetle.addHead());
+	}
+	
+	@Test
+	public void cannotAddSecondHead() {
+		beetle.addBody();
+		beetle.addHead();
+		assertFalse(beetle.addHead());		
+	}
+	
+	@Test
+	public void cannotAddTailWithoutBody() {
+		assertFalse(beetle.addTail());
 	}
 
 	@Test
-	public void testAddTail() {
-		assertFalse(beetle.hasTail());
-		// Can't add a tail without a body
-		assertFalse(beetle.addTail());
+	public void canAddTailWithBody() {
 		beetle.addBody();
 		assertTrue(beetle.addTail());
 		assertTrue(beetle.hasTail());
-		// Trying to add a second tail should fail
-		assertFalse(beetle.addTail());
+	}
+	
+	@Test
+	public void cannotAddSecondTail() {
+		beetle.addBody();
+		beetle.addTail();
+		assertFalse(beetle.addTail());		
 	}
 
 	@Test
-	public void testAddFeeler() {
-		assertEquals(0, beetle.getFeelers());
-		// Can't add feelers without a head
+	public void cannotAddFeelerWithoutHead() {
 		assertFalse(beetle.addFeeler());
+	}
+
+	@Test
+	public void canAddFeelerWithHead() {
 		beetle.addBody();
 		beetle.addHead();
 		assertTrue(beetle.addFeeler());
 		assertEquals(1, beetle.getFeelers());
+	}
+	
+	@Test
+	public void canAddSecondFeeler() {
+		beetle.addBody();
+		beetle.addHead();
+		beetle.addFeeler();
 		assertTrue(beetle.addFeeler());
 		assertEquals(2, beetle.getFeelers());
-		// Trying to add a third feeler should fail
+	}
+
+	@Test
+	public void cannotAddThirdFeeler() {
+		beetle.addBody();
+		beetle.addHead();
+		beetle.addFeeler();
+		beetle.addFeeler();
 		assertFalse(beetle.addFeeler());
 	}
 
 	@Test
-	public void testAddEye() {
-		assertEquals(0, beetle.getEyes());
-		// Can't add feelers without a head
-		assertFalse(beetle.addEye());
-		beetle.addBody();
-		beetle.addHead();
-		assertTrue(beetle.addEye());
-		assertEquals(1, beetle.getEyes());
-		assertTrue(beetle.addEye());
-		assertEquals(2, beetle.getEyes());
-		// Trying to add a third eye should fail
+	public void cannotAddEyeWithoutHead() {
 		assertFalse(beetle.addEye());
 	}
 
 	@Test
-	public void testAddLeg() {
-		assertEquals(0, beetle.getLegs());
-		// Can't add feelers without a head
-		assertFalse(beetle.addLeg());
+	public void canAddEyeWithHead() {
 		beetle.addBody();
 		beetle.addHead();
-		for (int i = 1; i <= 6; i++) {
-			assertTrue(beetle.addLeg());
-			assertEquals(i, beetle.getLegs());
-		}
-		// Trying to add a seventh leg should fail
-		assertFalse(beetle.addLeg());		
+		assertTrue(beetle.addEye());
+		assertEquals(1, beetle.getEyes());
 	}
 	
 	@Test
-	public void testIsComplete() {
+	public void canAddSecondEye() {
+		beetle.addBody();
+		beetle.addHead();
+		beetle.addEye();
+		assertTrue(beetle.addEye());
+		assertEquals(2, beetle.getEyes());
+	}
+
+	@Test
+	public void cannotAddThirdEye() {
+		beetle.addBody();
+		beetle.addHead();
+		beetle.addEye();
+		beetle.addEye();
+		assertFalse(beetle.addEye());
+	}
+
+	@Test
+	public void cannotAddLegWithoutHead() {
+		assertFalse(beetle.addLeg());
+	}
+
+	@Test
+	public void canAddLegWithHead() {
+		beetle.addBody();
+		beetle.addHead();
+		assertTrue(beetle.addLeg());
+		assertEquals(1, beetle.getLegs());
+	}
+	
+	@Test
+	public void canAddAdditionalLegs() {
+		beetle.addBody();
+		beetle.addHead();
+		for (int i = 0; i < 6; i++) {
+			assertTrue(beetle.addLeg());
+		}
+		assertEquals(6, beetle.getLegs());
+	}
+
+	@Test
+	public void cannotAddSeventhLeg() {
+		beetle.addBody();
+		beetle.addHead();
+		for (int i = 0; i < 6; i++) {
+			beetle.addLeg();
+		}
+		assertFalse(beetle.addLeg());
+	}
+	
+	@Test
+	public void detectsIncompleteBeetle() {
 		beetle.addBody();
 		beetle.addHead();
 		beetle.addTail();
@@ -102,7 +183,20 @@ public class BeetleTest {
 		beetle.addFeeler();
 		beetle.addFeeler();
 		beetle.addEye();
-		assertFalse(beetle.isComplete());
+		assertFalse(beetle.isComplete());		
+	}
+	
+	@Test
+	public void detectsCompleteBeetle() {
+		beetle.addBody();
+		beetle.addHead();
+		beetle.addTail();
+		for (int i = 0; i < 6; i++) {
+			beetle.addLeg();
+		}
+		beetle.addFeeler();
+		beetle.addFeeler();
+		beetle.addEye();
 		beetle.addEye();
 		assertTrue(beetle.isComplete());
 	}
--- Beetle/src/DieTest.java
+++ Beetle/src/DieTest.java
@@ -5,14 +5,13 @@ import org.junit.Test;
 public class DieTest {
 
 	@Test
-	public void testInitialTopFace() {
-		// A new die should have the 1 on top
+	public void initialTopFaceIs1() {
 		Die d = new Die();
 		assertEquals(1, d.getTopFace());
 	}
 
 	@Test
-	public void testRoll() {
+	public void allNumbersAreEquallyLikely() {
 		Die d = new Die();
 		int[] counts = new int[7];
 		// Roll the die many times
@@ -28,7 +27,24 @@ public class DieTest {
 	}
 
 	@Test
-	public void testMultipleDice() {
+	public void noExtraneousNumbersAppear() {
+		Die d = new Die();
+		int[] counts = new int[7];
+		// Roll the die many times
+		for (int i = 0; i < 600; i++) {
+			d.roll();
+			counts[d.getTopFace()]++;
+		}
+		// The 1 through 6 should account for all rolls
+		int sum = 0;
+		for (int i = 1; i <= 6; i++) {
+			sum += counts[i];
+		}
+		assertEquals(sum, 600);
+	}
+
+	@Test
+	public void multipleDiceCanShowDifferentTopFaces() {
 		Die d1 = new Die();
 		Die d2 = new Die();
 		int counter = 0;
