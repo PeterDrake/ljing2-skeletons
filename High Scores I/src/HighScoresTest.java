7716ace78c2986a03f178ed11cfcf3ed10d2031e
Peter Drake <drake@lclark.edu>
2016-06-09 17:24:21 -0700

    Updated tests and POGIL for High Scores I.

--- High Scores I/src/HighScoresTest.java
+++ High Scores I/src/HighScoresTest.java
@@ -7,14 +7,14 @@ public class HighScoresTest {
 	private int[] scores;
 
 	@Test
-	public void testInsertionSortEasy() {
+	public void insertionSortSortsSmallArray() {
 		scores = new int[] {6, 3, 4, 1, 5, 2};
 		HighScores.insertionSort(scores);
 		assertArrayEquals(new int[] {1, 2, 3, 4, 5, 6}, scores);
 	}
 
 	@Test
-	public void testInsertionSortHard() {
+	public void insertionSortSortsLargeArray() {
 		scores = new int[1000];
 		for (int i = 0; i < scores.length; i++) {
 			scores[i] = StdRandom.uniform(Integer.MAX_VALUE);
@@ -26,14 +26,14 @@ public class HighScoresTest {
 	}
 
 	@Test
-	public void testMergeSortEasy() {
+	public void mergeSortSortsSmallArray() {
 		scores = new int[] {6, 3, 4, 1, 5, 2};
 		HighScores.mergeSort(scores);
 		assertArrayEquals(new int[] {1, 2, 3, 4, 5, 6}, scores);
 	}
 	
 	@Test
-	public void testMergeSortHard() {
+	public void mergeSortSortsLargeArray() {
 		scores = new int[1000];
 		for (int i = 0; i < scores.length; i++) {
 			scores[i] = StdRandom.uniform(Integer.MAX_VALUE);
