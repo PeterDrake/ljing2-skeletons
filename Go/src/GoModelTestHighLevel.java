7bf3f7ef4c8d5b38de3977c85d9dbf9845d4ed5d
Peter Drake <drake@lclark.edu>
2016-06-14 13:58:23 -0700

    Updated tests and instructions for Go.

--- Go/src/GoModelTestHighLevel.java
+++ Go/src/GoModelTestHighLevel.java
@@ -13,7 +13,7 @@ public class GoModelTestHighLevel {
 	}
 
 	@Test
-	public void testPlay() {
+	public void playsCompleteGame() {
 		for (int[] moves : new int[][] { { 4, 4 }, { 5, 3 }, { 4, 3 }, { 5, 4 }, { 5, 5 }, { 6, 5 }, { 5, 6 }, { 6, 6 },
 				{ 5, 2 }, { 2, 6 }, { 2, 4 }, { 4, 7 }, { 5, 7 }, { 1, 5 }, { 1, 4 }, { 6, 2 }, { 6, 3 }, { 6, 4 },
 				{ 7, 3 }, { 7, 2 }, { 5, 1 }, { 6, 7 }, { 4, 8 }, { 3, 8 }, { 3, 7 }, { 5, 8 }, { 4, 6 }, { 4, 8 },
